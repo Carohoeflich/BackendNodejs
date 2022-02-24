2dd772787f455da0a65de7aa872a3ada05c8a740
Carolina Hoeflich <carohoeflich@hotmail.com>
2022-02-24 12:59:48 -0600

    some array operators examples with definitions

---
+++ sesion3/arrayOperators.js
@@ -0,0 +1,11 @@
+// Array.prototype.map : devuelve un nuevo array que contiene el resultado de llamar a la funcion pasada como parametro a todos los elementos del array sobre el que se invoca.
+
+// Array.prototype.filter : Devuelve un nuevo array que contiene todos los elementos de aquél para el cual se llama que cumplan el predicado que se le pasa como parámetro.
+// Array.prototype.find : Devuelve el primer elemento del array que cumpla el predicado que se pasa como parámetro, o undefined si ninguno lo cumple.
+// Array.prototype.findIndex : Devuelve el índice del primer elemento del array que cumpla el predicado que se pasa como parámetro, o -1 si nunguno lo cumple.
+// Array.prototype.fill : Asigna un valor estático a todos los elementos del array entre las posiciones inicio y fin
+// Array.prototype.copyWithin : Copia una secuencia de elementos de un array dentro del propio array.
+// Array.prototype.some : Devuelve true si al menos un elemento del array cumple con el predicado que se pasa como parámetro.
+// Array.prototype.every : Devuelve true si todos los elementos del array cumplen el predicado que recibe como parámetro.
+// Array.prototype.reduce : Aplica la función pasada como parámetro a un acumulador y a cada valor del array, que se recorre de izquierda a derecha, para reducirlo a un único valor.
+// Array.prototype.sort() : Ordena los elementos de un array, modificando éste, y devuelve el array ordenado.
